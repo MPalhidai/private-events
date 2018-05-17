5753a04bb7f2202839a6f40fe0f325242dd5e679
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-05-17 13:29:56 -0500

    Fixed user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:user) {FactoryBot.build(:user)}
+  let(:user) {FactoryBot.create(:user)}
 
   describe "Associations" do
     it "can create many events" do
-      first = user.events.create(host: user.id, location: "The restaurant at the end of the universe", date: Date.today)
-      second = user.events.create(host: user.id, location: "My place", date: Date.today)
+      first = user.events.create(creator: user, location: "The restaurant at the end of the universe", date: Date.today)
+      second = user.events.create(creator: user, location: "My place", date: Date.today)
       expect(first.nil?).to be false
       expect(second.nil?).to be false
-      expect(first.host).to eq(user.id)
-      expect(second.host).to eq(user.id)
+      expect(first.creator).to eq(user)
+      expect(second.creator).to eq(user)
     end
 
-    it "can attend many events" do
+    xit "can attend many events" do
       pending "How do I even?"
     end
   end
