a66f4df2d129052278bc7d798d28b6a72f6065c6
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-05-17 11:45:56 -0500

    First test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) {FactoryBot.build(:user)}
+
+  describe "Associations" do
+    it "can create many events" do
+      first = user.events.create(host: user.id, location: "The restaurant at the end of the universe", date: Date.today)
+      second = user.events.create(host: user.id, location: "My place", date: Date.today)
+      expect(first.nil?).to be false
+      expect(second.nil?).to be false
+      expect(first.host).to eq(user.id)
+      expect(second.host).to eq(user.id)
+    end
+
+    it "can attend many events" do
+      pending "How do I even?"
+    end
+  end
+
+
 end
