843a6f025eef3a946eed6e1edb648847b1092d62
Michael Palhidai <mpalhidai@gmail.com>
2018-05-29 14:29:10 -0700

    added bootstrap gem and required imports

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user_events' => 'users#show'
 end
