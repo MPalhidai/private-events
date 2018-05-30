f726ec97056dae1ff1434f053b8ff72b4841a5f6
Michael Palhidai <mpalhidai@gmail.com>
2018-05-30 13:27:55 -0700

    added events index view and controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,31 @@
 class EventsController < ApplicationController
+
+  def index
+    #all future events
+    Event.all
+  end
+
   def show
+    #one event
+    @event = Event.find(params[:id])
+		@guests = @event.guests
   end
 
   def create
+    #create new event
+  end
+
+  def destroy
+    #create new event
+  end
+
+  def update
+    #create new event
+  end
+
+  private
+
+  def event_params
+	   params.require(:event).permit(:title, :description, :location, :event_date, :start_time)
   end
 end
