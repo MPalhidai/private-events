4e3f66071c660cc15934d9d8f8b8b2f3c78f00b0
Michael Palhidai <mpalhidai@gmail.com>
2018-06-05 12:21:33 -0700

    refactored appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,23 @@
 class AppointmentsController < ApplicationController
+
   def create
-    event = Event.find_by(id: params[:appointment][:attended_event_id])
-  	current_user.appointments.create(attended_event_id: event.id, attendee_id: current_user.id)
-  	redirect_to request.referrer
+    @appointment = current_user.appointments.build(appointment_params)
+    if @appointment.save
+      redirect_to request.referrer
+    else
+      render :new
+    end
   end
 
   def destroy
   	@appointment = Appointment.find(params[:id])
   	current_user.appointments.destroy(@appointment)
   	redirect_to request.referrer
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:attended_event_id, :attendee_id)
   end
 end
