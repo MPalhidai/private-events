6a3ec4c71e2497110b653c453f55d570dab10a2a
Michael Palhidai <mpalhidai@gmail.com>
2018-06-04 21:25:33 -0700

    appointment form for only writes to even_id 1 and can add the same user to an event multiple times

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentsController < ApplicationController
   def create
     @event = Event.find(params[:appointment][:attended_event_id])
-  	current_user.appointments.build(attended_event_id: @event.id)
+  	current_user.appointments.create(attended_event_id: @event.id, attendee_id: current_user.id, )
   	redirect_to request.referrer
   end
 
