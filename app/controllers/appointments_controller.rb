2acbf18a470ea4f9f9da8c366e378608ac9281a8
Michael Palhidai <mpalhidai@gmail.com>
2018-06-11 13:06:52 -0700

    form without rewriting paramsand non-nested routes

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,23 +1,14 @@
 class AppointmentsController < ApplicationController
 
-  def create
-    @appointment = current_user.appointments.build(appointment_params)
-    if @appointment.save
-      redirect_to request.referrer
-    else
-      render :new
-    end
+def create
+    @event = Event.find(params[:appointment][:attended_event_id])
+    current_user.appointments.create(attended_event_id: @event.id)
+    redirect_to request.referrer
   end
 
   def destroy
     @appointment = Appointment.find(params[:id])
     current_user.appointments.destroy(@appointment)
     redirect_to request.referrer
-  end
-
-  private
-
-  def appointment_params
-    params.require(:appointment).permit(:attended_event_id, :attendee_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   get '/user_events', to: 'users#show'
   resources :users, except: [:index]
-  resources :events do
-    resources :appointments, only: [:create, :destroy]
-  end
+  resources :events
+  resources :appointments, only: [:create, :destroy]
 end
